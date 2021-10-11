e88239e9d7a2f91441d9b74739a296f6abce9b33
Ezequiel carrizo <ezequielce490@gmail.com>
2021-10-11 19:40:31 -0300

    complete function game

--- rock.js
+++ rock.js
@@ -24,36 +24,53 @@ const playRound = function (playerC, computerC) {
   //rock
 
   if (playerC === "rock" && computerC === "scissor") {
+    playerScore++;
     return `Player win, ${playerC} beats ${computerC}`;
   }
   if (playerC === "rock" && computerC === "paper") {
+    computerScore++;
     return `Player Lose, ${playerC} lose to ${computerC}`;
   }
 
   //paper
 
   if (playerC === "paper" && computerC === "rock") {
+    playerScore++;
     return `Player win, ${playerC} beats ${computerC}`;
   }
   if (playerC === "paper" && computerC === "scissor") {
+    computerScore++;
     return `Player Lose, ${playerC} lose to ${computerC}`;
   }
 
   //scissor
 
   if (playerC === "scissor" && computerC === "paper") {
+    playerScore++;
     return `Player win, ${playerC} beats ${computerC}`;
   }
   if (playerC === "scissor" && computerC === "rock") {
+    computerScore++;
     return `Player Lose, ${playerC} lose to ${computerC}`;
   }
 };
 
-//elecciones
+//juego
 
-const playerChoice = prompt("choice Rock,Paper or Scissor").toLowerCase();
-const computer_Choice = computerPlay().toLowerCase();
+const game = function () {
+  //loop
+  for (let i = 1; i <= 5; i++) {
+    //elecciones
 
-console.log(`player ${playerChoice} / cpu ${computer_Choice}`);
+    const playerChoice = prompt("choice Rock,Paper or Scissor").toLowerCase();
+    const computer_Choice = computerPlay().toLowerCase();
+    console.log(playRound(playerChoice, computer_Choice));
+    console.log(`player score: ${playerScore} / cpu score: ${computerScore}`);
+    console.log("++++++++++++++++++++++++++++++++++++++++++");
+  }
+};
 
-console.log(playRound(playerChoice, computer_Choice));
+let playerScore = 0;
+let computerScore = 0;
+
+game();
